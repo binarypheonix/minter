a25d66c08d68b447e223a9eaefc249504c11abb4
binarypheonix <21365550+binarypheonix@users.noreply.github.com>
2021-07-13 16:08:03 +0530

    Updates

---
+++ precache-manifest.299ab49ea522baa5d56166775566499e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7de652773eab712a4acd13e363768337",
+    "url": "/minter/index.html"
+  },
+  {
+    "revision": "89fc156ddf26e3bc5813",
+    "url": "/minter/static/css/main.4af018f7.chunk.css"
+  },
+  {
+    "revision": "e08d39b30e4f93b8e8c1",
+    "url": "/minter/static/js/2.aabe2725.chunk.js"
+  },
+  {
+    "revision": "3718db4aeb0f4e578d2ea3a4c8b6fd69",
+    "url": "/minter/static/js/2.aabe2725.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89fc156ddf26e3bc5813",
+    "url": "/minter/static/js/main.e1482ad1.chunk.js"
+  },
+  {
+    "revision": "b3b116c7ff6d8aa2a4bf",
+    "url": "/minter/static/js/runtime-main.b61e28f6.js"
+  },
+  {
+    "revision": "a9dd48a803457026ce0159f2e32567bc",
+    "url": "/minter/static/media/header-logo.a9dd48a8.svg"
+  },
+  {
+    "revision": "8cce1dcbdb9e8aa0871f25d0092cc074",
+    "url": "/minter/static/media/link-icon.8cce1dcb.svg"
+  },
+  {
+    "revision": "d899201254e9b34d0708a0af1db9c642",
+    "url": "/minter/static/media/splash-logo.d8992012.svg"
+  },
+  {
+    "revision": "b5be31f680217869cb6dd73e5cb4c66c",
+    "url": "/minter/static/media/tezos-sym.b5be31f6.svg"
+  },
+  {
+    "revision": "1a72e9f43d27014cd6a4abb3ac4b9cca",
+    "url": "/minter/static/media/wallet.1a72e9f4.svg"
+  }
+]);
